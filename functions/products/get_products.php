480182bca5e258def1565af7ee784b39d0070bf8
kececihasan <ghasankececi@gmail.com>
2025-01-16 14:32:28 +0300

    a

--- functions/products/get_products.php
+++ functions/products/get_products.php
@@ -22,7 +22,7 @@ $searchValue = $_POST['search']['value'];
 
 // Build the query
 $query = "
-    SELECT p.id, p.UrunKodu, p.UrunAdiTR, m.id AS mid, m.title, c.KategoriAdiTR AS category_name, p.Vitrin, p.YeniUrun, p.aktif
+    SELECT SQL_CALC_FOUND_ROWS p.id, p.UrunKodu, p.UrunAdiTR, m.id AS mid, m.title, c.KategoriAdiTR AS category_name, p.Vitrin, p.YeniUrun, p.aktif
     FROM nokta_urunler p
     LEFT JOIN nokta_kategoriler c ON p.KategoriID = c.id
     LEFT JOIN nokta_urun_markalar AS m ON m.id = p.MarkaID
@@ -46,22 +46,16 @@ $totalRecordsQuery = "SELECT COUNT(*) as total FROM nokta_urunler";
 $totalRecordsResult = $database->fetchAll($totalRecordsQuery);
 $totalRecords = $totalRecordsResult[0]['total'];
 
-// Get filtered records count
-$filteredRecordsQuery = $query;
-$filteredRecordsResult = $database->fetchAll($filteredRecordsQuery, $params);
-$totalFilteredRecords = count($filteredRecordsResult);
-
 // Add pagination
 $query .= " LIMIT " . intval($start) . ", " . intval($length);
 
-// Debugging: Output the final query and parameters
-error_log('Final query: ' . $query);
-error_log('Parameters: ' . print_r($params, true));
-
+// Get the results
 $results = $database->fetchAll($query, $params);
 
-// Debugging: Output memory usage
-error_log('Memory usage before processing results: ' . memory_get_usage());
+// Get total filtered records
+$filteredRecordsQuery = "SELECT FOUND_ROWS() as total";
+$filteredResult = $database->fetchAll($filteredRecordsQuery);
+$totalFilteredRecords = $filteredResult[0]['total'];
 
 // Prepare data for DataTables
 $data = [];
