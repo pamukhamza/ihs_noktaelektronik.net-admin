653f92bcc22928728ee9e42616ffc3d79407678b
kececihasan <ghasankececi@gmail.com>
2025-01-30 14:32:37 +0300

    a

--- functions/web_net/offer/fetch_offer_details.php
+++ functions/web_net/offer/fetch_offer_details.php
@@ -4,7 +4,7 @@ include_once '../../db.php';
 if (isset($_GET['id'])) {
     $id = intval($_GET['id']); // Sanitize input
     $database = new Database();
-    $query = "SELECT nef.*,nu.id,nu.UrunAdiTr FROM net_offers AS nef WHERE nef.id = $id
+    $query = "SELECT nef.*,nu.id,nu.UrunAdiTR FROM net_offers AS nef WHERE nef.id = $id
     LEFT JOIN nokta_urunler AS nu ON nef.prod_id = nu.id
     ";
     $result = $database->fetch($query);
@@ -16,7 +16,7 @@ if (isset($_GET['id'])) {
         echo '<p><strong>Telefon:</strong> ' . htmlspecialchars($result['phone']) . '</p>';
         echo '<p><strong>E-Posta:</strong> ' . htmlspecialchars($result['mail']) . '</p>';
         echo '<p><strong>Tarih:</strong> ' . htmlspecialchars($result['date']) . '</p>';
-        echo '<p><strong>Tarih:</strong> ' . htmlspecialchars($result['UrunAdiTr']) . '</p>';
+        echo '<p><strong>Tarih:</strong> ' . htmlspecialchars($result['UrunAdiTR']) . '</p>';
         echo '<p><strong>Açıklama:</strong> ' . htmlspecialchars($result['description']) . '</p>';
     } else {
         echo '<p>No data found for this ID.</p>';
