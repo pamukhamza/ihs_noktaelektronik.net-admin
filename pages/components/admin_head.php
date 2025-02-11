be27a3dfc5aec2a37b9d33e3ea14870b68cd6b39
kececihasan <ghasankececi@gmail.com>
2025-02-11 15:16:13 +0300

    a

--- pages/components/admin_head.php
+++ pages/components/admin_head.php
@@ -18,7 +18,7 @@ checkSession();
 <!DOCTYPE html>
 <html lang="en" class="light-style layout-navbar-fixed layout-menu-fixed layout-compact " dir="ltr" data-theme="theme-default" data-assets-path="../assets/" data-template="vertical-menu-template" data-style="light">
 <head>
-    <base href="http://192.168.16.63/noktanet_admin/">
+    <base href="https://www.noktaelektronik.net/admin/">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
     <title><?= $title; ?></title>
