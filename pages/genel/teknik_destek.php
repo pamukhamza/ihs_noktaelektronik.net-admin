bdf60b28d4344c369f07ac5acf378594a9fd9f02
pamukhamza <hmzpmk34@gmail.com>
2025-04-14 16:46:56 +0300

    a

--- pages/genel/teknik_destek.php
+++ pages/genel/teknik_destek.php
@@ -412,6 +412,8 @@ $durumlar = $database->fetchAll("SELECT * FROM nokta_teknik_durum");
         </div>
     </div>
     <!-- Core JS -->
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 <script src="assets/vendor/libs/jquery/jquery.js"></script>
 <script src="assets/vendor/js/bootstrap.js"></script>
 <script src="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
