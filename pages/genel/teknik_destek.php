f10410bc35ef33476daadc0378f5231d091edf7c
pamukhamza <hmzpmk34@gmail.com>
2025-04-14 16:37:57 +0300

    a

--- pages/genel/teknik_destek.php
+++ pages/genel/teknik_destek.php
@@ -730,3 +730,186 @@ $durumlar = $database->fetchAll("SELECT * FROM nokta_teknik_durum");
         });
     });
 </script>
+<script>
+     $(document).ready(function() {
+        $('.sozBtn').click(function() {
+            $('#sozlesmeModal').modal('show');
+        });
+        $('.sozOnay').click(function() {
+            $("#onay").prop("checked", true);
+            $('#sozlesmeModal').modal('hide');
+        });
+     });
+</script>
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var gonderimSekliSelect = document.getElementById("gonderim_sekli");
+        var kargoFirmasiDiv = document.getElementById("kargo_firmasi_div");
+        var kargoFirmasiInput = document.getElementById("kargo_firmasi");
+
+        // Initial check
+        toggleKargoFirmasiVisibility();
+
+        // Event listener for gonderim_sekli change
+        gonderimSekliSelect.addEventListener("change", function() {
+            toggleKargoFirmasiVisibility();
+        });
+
+        // Function to toggle kargo_firmasi visibility
+        function toggleKargoFirmasiVisibility() {
+            if (gonderimSekliSelect.value === "1") {
+                kargoFirmasiDiv.style.display = "block";
+                kargoFirmasiInput.setAttribute("required", "required");
+            } else {
+                kargoFirmasiDiv.style.display = "none";
+                kargoFirmasiInput.removeAttribute("required");
+            }
+        }
+    });
+</script>
+<script>
+    $(document).ready(function() {
+        $('.basvur-btn').click(function() {
+            $('#basvuruModal').modal('show');
+        });
+        // Yeni giriş satırı ekleme işlevi
+        function addInputRow() {
+            var newRow = $('#input-row-template').clone().removeAttr('id').removeAttr('style');
+            $('#input-rows-container').append(newRow);
+        }
+        // İlk satır ekleme olayı dinleyicisi
+        $(document).on('click', '.add-row-btn', function() {
+            addInputRow();
+        });
+        // Satır silme olayı dinleyicisi
+        $(document).on('click', '.remove-row-btn', function() {
+            $(this).closest('.row').remove();
+        });
+        $('#applicationForm').submit(function(e) {
+            e.preventDefault();
+
+            var urun_kodu_array = [];
+            var seri_no_array = [];
+            var adet_array = [];
+
+            $('#input-rows-container .row').each(function() {
+                var urun_kodu = $(this).find('.urun_kodu').val();
+                var seri_no = $(this).find('.seri_no').val();
+                var adet = $(this).find('.adet').val();
+
+                urun_kodu_array.push(urun_kodu);
+                seri_no_array.push(seri_no);
+                adet_array.push(adet);
+            });
+
+            var formData = new FormData();
+            formData.append('urun_kodu', urun_kodu_array.join(','));
+            formData.append('seri_no', seri_no_array.join(','));
+            formData.append('adet', adet_array.join(','));
+            // Diğer form verilerini ekleyin
+            formData.append('id', $('#musteri_id').val());
+            formData.append('musteri', $('#musteri').val());
+            formData.append('tel', $('#tel').val());
+            formData.append('email', $('#email').val());
+            formData.append('adres', $('#adres').val());
+            formData.append('fatura_no', $('#fatura_no').val());
+            formData.append('aciklama', $('#aciklama').val());
+            formData.append('ad_soyad', $('#ad_soyad').val());
+            formData.append('onay', $('#onay').is(':checked') ? 1 : 0);
+            formData.append('gonderim_sekli', $('#gonderim_sekli').val());
+            formData.append('kargo_firmasi', $('#kargo_firmasi').val());
+            formData.append('type', 'ariza');
+
+            $.ajax({
+                type: 'POST',
+                url: 'functions/edit_info.php',
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function(gelen) {
+                    $('#basvuruModal').modal('hide');
+
+                    // Modal içeriğini ayarlayın
+                    $('#modalTitle').text("Başvurunuz Alınmıştır!");
+                    $('#modalBody').html('Arıza Takip Kodunuz: ' + gelen);
+                    $('#successModal').modal('show');
+
+                    $('#yazdirButton').off('click').on('click', function() {
+                        var urun_kodu = urun_kodu_array.join(', ');
+                        var seri_no = seri_no_array.join(', ');
+                        var adet = adet_array.join(', ');
+                        var musteri = $('#musteri').val();
+                        var tel = $('#tel').val();
+                        var email = $('#email').val();
+                        var aciklama = $('#aciklama').val();
+
+                        var printContent = `
+                    <html>
+                    <head>
+                        <title>Yazdır</title>
+                        <style>
+                            body {
+                                font-family: Arial, sans-serif;
+                                margin: 20px;
+                            }
+                            h3 {
+                                color: #333;
+                            }
+                            p {
+                                font-size: 14px;
+                                line-height: 1.6;
+                            }
+                            strong {
+                                color: #555;
+                            }
+                            .header {
+                                text-align: center;
+                                margin-bottom: 20px;
+                            }
+
+                        </style>
+                    </head>
+                    <body>
+                        <div class="header">
+                            <h3>Başvuru Bilgileri</h3>
+                        </div>
+                        <p><strong>Takip Kodu:</strong> ${gelen}</p>
+                        <p><strong>Ürün Kodu:</strong> ${urun_kodu}</p>
+                        <p><strong>Seri No:</strong> ${seri_no}</p>
+                        <p><strong>Adet:</strong> ${adet}</p>
+                        <p><strong>Müşteri:</strong> ${musteri}</p>
+                        <p><strong>Telefon:</strong> ${tel}</p>
+                        <p><strong>Email:</strong> ${email}</p>
+                        <p><strong>Açıklama:</strong> ${aciklama}</p>
+
+                    </body>
+                    </html>
+                `;
+
+                        var newWindow = window.open('', '', 'width=600,height=400');
+                        newWindow.document.write(printContent);
+                        newWindow.document.close();
+                        newWindow.onload = function() {
+                            newWindow.print();
+                            newWindow.onafterprint = function() {
+                                newWindow.close();
+                            };
+                        };
+                    });
+                },
+                error: function(response) {
+                    if (response.status === 400) {
+                        alert("Lütfen zorunlu alanları doldurunuz !");
+                    }
+                    if (response.status === 500) {
+                        alert("Hatalı e-posta adresi !");
+                    }
+                    if (response.status === 600) {
+                        alert("Lütfen Kargo Firmasını Doldurunuz !");
+                    }
+                }
+            });
+        });
+        
+    });
+</script>
