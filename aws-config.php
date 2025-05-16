c94b5c7f78c0cd8616626916254ba4fdd7edb8c1
Hamza Pamuk <hmzpmk34@gmail.com>
2025-05-16 12:38:09 +0300

    aws config

--- aws-config.php
+++ aws-config.php
@@ -2,7 +2,7 @@
 return [
     's3' => [
         'key'    => 'AKIAWNHTHJK4LGVYEKUL',
-        'secret' => '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',
+        'secret' => 'akjotpUR5tiGvl/Np60x+lbrcvSHPaRmLJl1ih3I/MD5',
         'bucket' => 'noktanet',
         'region' => 'eu-central-1',
     ],
