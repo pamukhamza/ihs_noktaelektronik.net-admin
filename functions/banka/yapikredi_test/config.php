c0770249d96acb83b2d3813e89bec09c7cf71dfb
Hamza Pamuk <hmzpmk34@gmail.com>
2025-05-21 10:59:16 +0300

    yapikredi

--- functions/banka/yapikredi_test/config.php
+++ functions/banka/yapikredi_test/config.php
@@ -1,7 +1,7 @@
 <?php
 // Ortam değişkenleri - TEST ortamı bilgileri
-define('MERCHANT_ID', '6700000001');
-define('TERMINAL_ID', '67000001');
-define('POSNET_ID', '9644');
+define('MERCHANT_ID', '6700972656');
+define('TERMINAL_ID', '67C31965');
+define('POSNET_ID', '1010074357974941');
 define('POSNET_URL', 'https://setmpos.ykb.com/PosnetWebService/XML');
 ?>
