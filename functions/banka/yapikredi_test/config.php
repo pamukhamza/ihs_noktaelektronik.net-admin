363a79de3e3a558e1cccc03f3efac95dc72ed0b5
Hamza Pamuk <hmzpmk34@gmail.com>
2025-05-23 14:29:19 +0300

    yapi kredi

--- functions/banka/yapikredi_test/config.php
+++ functions/banka/yapikredi_test/config.php
@@ -1,7 +1,7 @@
 <?php
 // Ortam değişkenleri - TEST ortamı bilgileri
-define('MERCHANT_ID', '6700972656');
-define('TERMINAL_ID', '67C31965');
-define('POSNET_ID', '1010074357974941');
+define('MERCHANT_ID', '6700972665');//6700972656
+define('TERMINAL_ID', '67C31966');//67C31965
+define('POSNET_ID', '1010074357971913');//1010074357974941
 define('POSNET_URL', 'https://setmpos.ykb.com/PosnetWebService/XML');
 ?>
