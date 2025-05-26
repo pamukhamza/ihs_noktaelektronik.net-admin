cf86e4be0a00428eebb0e6444b74939891bd3e5c
Hamza Pamuk <hmzpmk34@gmail.com>
2025-05-26 15:40:34 +0300

    sanalposapi

--- functions/banka/yapikredi_test/config.php
+++ functions/banka/yapikredi_test/config.php
@@ -3,6 +3,6 @@ define('MERCHANT_ID', '6702493366');
 define('TERMINAL_ID', '67C242C2');
 define('POSNET_ID', '1010651186111175');
 define('POSNET_URL', 'https://posnet.yapikredi.com.tr/PosnetWebService/XML');
-define('ENCKEY', '34.34.84.84.54.59.59.41');
+define('ENCKEY', '28.73.66.34.54.59.29.17');
 
 ?>
