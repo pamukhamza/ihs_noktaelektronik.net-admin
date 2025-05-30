ca0eb20ef52a70f81a42532c03926b49c7167897
Hamza Pamuk <hmzpmk34@gmail.com>
2025-05-30 15:05:49 +0300

    banner

--- pages/b2b/b2b-banner.php
+++ pages/b2b/b2b-banner.php
@@ -109,7 +109,7 @@ $template->head();
                                                     ?>
                                                         <tr>
                                                             <td><?= $row["id"] ?></td>
-                                                            <td style="max-width: 650px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;"><?= $row["link1"] ?></td>
+                                                            <td style="max-width: 300px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;"><?= $row["link1"] ?></td>
                                                             <td>
                                                                 <ul class="list-unstyled m-0 avatar-group d-flex align-items-center">
                                                                     <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top">
@@ -117,7 +117,7 @@ $template->head();
                                                                     </li>
                                                                 </ul>
                                                             </td>
-                                                            <td style="max-width: 650px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;"><?= $row["link2"] ?></td>
+                                                            <td style="max-width: 300px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;"><?= $row["link2"] ?></td>
                                                             <td>
                                                                 <ul class="list-unstyled m-0 avatar-group d-flex align-items-center">
                                                                     <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top">
@@ -125,7 +125,7 @@ $template->head();
                                                                     </li>
                                                                 </ul>
                                                             </td>
-                                                            <td style="max-width: 650px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;"><?= $row["link3"] ?></td>
+                                                            <td style="max-width: 300px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;"><?= $row["link3"] ?></td>
                                                             <td>
                                                                 <ul class="list-unstyled m-0 avatar-group d-flex align-items-center">
                                                                     <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top">
