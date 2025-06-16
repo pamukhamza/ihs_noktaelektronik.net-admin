ede71104c7eadb0817c05ed54ebb60707bcf4ac9
Hamza Pamuk <hmzpmk34@gmail.com>
2025-06-16 11:51:15 +0300

    db saat

--- functions/db.php
+++ functions/db.php
@@ -16,6 +16,7 @@ class Database {
         try {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8", $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+             $this->conn->exec("SET time_zone = '+03:00'");
         } catch(PDOException $e) {
             echo "Connection error: " . $e->getMessage();
         }
