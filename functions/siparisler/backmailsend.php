9b6c4ed5b7a106533fb45b9e077d70b9eba4a4fc
Hamza Pamuk <hmzpmk34@gmail.com>
2025-07-03 15:48:49 +0300

    siparisler buton

--- functions/siparisler/backmailsend.php
+++ functions/siparisler/backmailsend.php
@@ -6,6 +6,7 @@ $konu = $_POST['konu'];
 $icerik = $_POST['icerik'];
 $baslik = $_POST['baslik'];
 
-mailGonder($alici, $konu, $icerik, $baslik);
+//mailGonder($alici, $konu, $icerik, $baslik);
+mailGonder('hmzpmk34@gmail.com', 'deneme konusu', 'yok', 'baslik');
 echo "geldi";
 ?>
