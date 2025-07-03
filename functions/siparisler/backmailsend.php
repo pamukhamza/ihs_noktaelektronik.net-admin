dc30c4d37d75b4bd149cfa7ea0db16fdba799e26
Hamza Pamuk <hmzpmk34@gmail.com>
2025-07-03 15:50:40 +0300

    siparisler buton

--- functions/siparisler/backmailsend.php
+++ functions/siparisler/backmailsend.php
@@ -1,10 +1,10 @@
 <?php
 require '../../mail/mail_gonder.php'; // mailGonder ve diğer fonksiyonlar burada tanımlıysa
 
-$alici = $_POST['alici'];
-$konu = $_POST['konu'];
-$icerik = $_POST['icerik'];
-$baslik = $_POST['baslik'];
+//$alici = $_POST['alici'];
+//$konu = $_POST['konu'];
+//$icerik = $_POST['icerik'];
+//$baslik = $_POST['baslik'];
 
 //mailGonder($alici, $konu, $icerik, $baslik);
 mailGonder('hmzpmk34@gmail.com', 'deneme konusu', 'yok', 'baslik');
