dd64bfb581a905444749b3b8cd70c217730250a6
Hamza Pamuk <hmzpmk34@gmail.com>
2025-07-03 15:47:27 +0300

    siparisler buton

--- functions/siparisler/backmailsend.php
+++ functions/siparisler/backmailsend.php
@@ -1,10 +1,11 @@
 <?php
 require '../../mail/mail_gonder.php'; // mailGonder ve diğer fonksiyonlar burada tanımlıysa
-echo "geldi";
+
 $alici = $_POST['alici'];
 $konu = $_POST['konu'];
 $icerik = $_POST['icerik'];
 $baslik = $_POST['baslik'];
 
 mailGonder($alici, $konu, $icerik, $baslik);
+echo "geldi";
 ?>
