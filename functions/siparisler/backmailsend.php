335fda658d3a35212802a386c6f198022c8ca578
Hamza Pamuk <hmzpmk34@gmail.com>
2025-07-03 16:08:22 +0300

    siparisler buton

--- functions/siparisler/backmailsend.php
+++ functions/siparisler/backmailsend.php
@@ -1,12 +1,14 @@
 <?php
+file_put_contents(__DIR__ . '/mail_debug.log', "Başladı\n", FILE_APPEND);
+parse_str(file_get_contents("php://input"), $post);
+file_put_contents(__DIR__ . '/mail_debug.log', print_r($post, true), FILE_APPEND);
 require '../../mail/mail_gonder.php'; // mailGonder ve diğer fonksiyonlar burada tanımlıysa
 
-//$alici = $_POST['alici'];
-//$konu = $_POST['konu'];
-//$icerik = $_POST['icerik'];
-//$baslik = $_POST['baslik'];
+$alici = $_POST['alici'];
+$konu = $_POST['konu'];
+$icerik = $_POST['icerik'];
+$baslik = $_POST['baslik'];
 
-//mailGonder($alici, $konu, $icerik, $baslik);
-mailGonder('hmzpmk34@gmail.com', 'deneme konusu', 'yok', 'baslik');
-echo "geldi";
+mailGonder($alici, $konu, $icerik, $baslik);
+file_put_contents(__DIR__ . '/mail_debug.log', "Mail gönderildi\n", FILE_APPEND);
 ?>
